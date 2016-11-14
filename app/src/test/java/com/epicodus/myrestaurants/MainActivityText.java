67fd155808ca0cbb6374437c667c141e91f7860a
tj2001 <jung.m.tim@gmail.com>
2016-11-13 17:17:34 -0800

    create MainActivityTest.java file

---
+++ app/src/test/java/com/epicodus/myrestaurants/MainActivityText.java
@@ -0,0 +1,20 @@
+package com.epicodus.myrestaurants;
+
+import android.os.Build;
+
+import org.junit.runner.RunWith;
+
+
+@Config(constants = BuildConfig.class, sdk = Build.VERSION_CODES.LOLLIPOP)
+@RunWith(RobolectricGradleTestRunner.class)
+public class MainActivityTest {
+
+}
+
+/**
+ * Created by Guest on 11/13/16.
+ */
+public class MainActivityText {
+
+
+}
